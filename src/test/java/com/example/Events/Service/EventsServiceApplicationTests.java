42a2379a1aae9d901ba3683dce99cb38ec3f1129
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-01-30 16:30:35 +0300

    Initial commit

---
+++ src/test/java/com/example/Events/Service/EventsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Events.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
